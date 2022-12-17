35c23160d49e7ebad911a42f6b8e45bdf266c893
Oleksandr Syrotenko <syrotenko.om@gmail.com>
2022-12-17 21:24:33 +0200

    Add app zones & error catcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,98 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:catcher/catcher.dart';
+// import 'services/catcher_firebase.dart';
+
+import 'shared/app_vars.dart';
+import 'shared/constants.dart';
+// import 'services/sqlite_db.dart';
+import 'myapp.dart';
 
 void mainDelegate() => appMain();
 
-void appMain() {
-  runApp(const MyApp());
-}
+// ignore: prefer_void_to_null
+Future<Null> appMain() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (Environment().config.isItDev) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current
+          .handleUncaughtError(details.exception, details.stack as StackTrace);
+    }
+  };
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // ignore: prefer_void_to_null
+  runZonedGuarded<Future<Null>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      //await SqliteDb.init();
+      String catcherScreenShots = await myCatcherScreenShotPath();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+      final CatcherOptions debugOptions = CatcherOptions(
+        PageReportMode(showStackTrace: true),
+        [
+          // CrashlyticsHandler(
+          //   enableDeviceParameters: true,
+          //   enableApplicationParameters: true,
+          //   enableCustomParameters: true,
+          //   printLogs: true
+          // ),
+          ConsoleHandler(
+              enableApplicationParameters: true,
+              enableDeviceParameters: true,
+              enableCustomParameters: true,
+              enableStackTrace: true),
+        ],
+        screenshotsPath: catcherScreenShots,
+      );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      final CatcherOptions releaseOptions = CatcherOptions(DialogReportMode(), [
+        EmailManualHandler(
+          [
+            catcherEmailAddrOne,
+            catcherEmailAddrTwo,
           ],
+          enableDeviceParameters: true,
+          enableStackTrace: true,
+          enableCustomParameters: true,
+          enableApplicationParameters: true,
+          sendHtml: true,
+          emailTitle: appName,
+          emailHeader: "Error Report",
+          printLogs: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ]);
+
+      final GlobalKey<NavigatorState> navigatorKey =
+          GlobalKey<NavigatorState>();
+      Catcher(
+        rootWidget: const MyApp(),
+        debugConfig: debugOptions,
+        releaseConfig: releaseOptions,
+        navigatorKey: navigatorKey,
+      );
+    },
+    (Object error, StackTrace stack) {},
+    zoneSpecification: ZoneSpecification(
+      print: (self, parent, zone, line) async {
+        final messageToLog = "[${DateTime.now()}] $appName $line $zone";
+        parent.print(zone, messageToLog);
+      },
+    ),
+  );
+}
+
+Future<String> myCatcherScreenShotPath() async {
+  late Directory externalDir;
+  if (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia) {
+    externalDir = await getExternalStorageDirectory() as Directory;
   }
+  if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+    externalDir = await getApplicationDocumentsDirectory();
+  }
+  String path = externalDir.path.toString();
+  return path;
 }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,5 @@
+//saved preferences
+const String catcherEmailAddrOne = "a1exs@ukr.net";
+const String catcherEmailAddrTwo = "alex.sirotenko@gmail.com";
+
+const String appName = "My Health Diary";
