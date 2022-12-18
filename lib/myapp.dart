a3c6348470165843628c8c43c546bbe7c6c6478b
Oleksandr Syrotenko <syrotenko.om@gmail.com>
2022-12-18 19:57:12 +0200

    Prepare for Future data & navigator use

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -4,6 +4,7 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'pages/home_page.dart';
 import 'shared/globals.dart' as globals;
 
 class MyApp extends StatefulWidget {
@@ -14,119 +15,68 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
+  bool? _canStartApp;
+
+  @override
+  void dispose() {
+    globals.settingsManager.removeListener(settingsListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    globals.settingsManager.initTheme();
+    globals.settingsManager.initLocale();
+    globals.settingsManager.addListener(settingsListener);
+    super.initState();
+
+    initFutureData().then((value) {
+      _canStartApp = true;
+      setState(() {});
+    });
+  }
+
+  void settingsListener() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> initFutureData() async {}
+
   @override
   Widget build(BuildContext context) {
     FlexScheme usedScheme = FlexScheme.aquaBlue;
 
-    return MaterialApp(
-        navigatorKey: Catcher.navigatorKey,
-        onGenerateTitle: (context) {
-          return AppLocalizations.of(context)!.appTitle;
-        },
-        //locale
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          // delegate from flutter_localization
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: globals.settingsManager.supportedLocales,
-        localeResolutionCallback: (locale, suppLocales) {
-          return globals.settingsManager.localeResolution(locale);
-        },
-        locale: globals.settingsManager.selectedLocale,
-        //theme
-        theme: FlexThemeData.light(scheme: usedScheme),
-        darkTheme: FlexThemeData.dark(scheme: usedScheme),
-        themeMode: ThemeMode.system,
-        home: CatcherScreenshot(
-          catcher: Catcher.getInstance(),
-          child: const MyHomePage(),
-        ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(AppLocalizations.of(context)!.appTitle),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return (_canStartApp == null)
+        ? MaterialApp(
+            theme: FlexThemeData.light(scheme: usedScheme),
+            darkTheme: FlexThemeData.dark(scheme: usedScheme),
+            themeMode: ThemeMode.system,
+            home: const Scaffold(),
+          )
+        : MaterialApp(
+            navigatorKey: Catcher.navigatorKey,
+            onGenerateTitle: (context) {
+              return AppLocalizations.of(context)!.appTitle;
+            },
+            //locale
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              // delegate from flutter_localization
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: globals.settingsManager.supportedLocales,
+            localeResolutionCallback: (locale, suppLocales) {
+              return globals.settingsManager.localeResolution(locale);
+            },
+            locale: globals.settingsManager.selectedLocale,
+            //theme
+            theme: FlexThemeData.light(scheme: usedScheme),
+            darkTheme: FlexThemeData.dark(scheme: usedScheme),
+            themeMode: ThemeMode.system,
+            home: const MyHomePage());
   }
 }
